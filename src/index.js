2ca6e065282a6a575940dc1561be758db5d53344
Arpit Yadav <yadavification@gmail.com>
2023-07-06 15:03:03 +0530

    removed browserrouter

--- src/index.js
+++ src/index.js
@@ -10,10 +10,8 @@ import Navbar from './Components/NavBar';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
     <Navbar/>
     <App />
-  </BrowserRouter>,
   </React.StrictMode>
 );
 
